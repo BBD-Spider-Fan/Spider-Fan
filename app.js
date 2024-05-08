82163aad545ea6a3e3c37cc7e89722039e4f4da4
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-05-08 20:59:14 +0200

    Added back stuff

--- app.js
+++ app.js
@@ -14,13 +14,11 @@ const app = express();
 // Creates a logger for us uses a package so will remove later.
 // app.use(logger('dev'));
 
-/*
 if (process.env.NODE_ENV !== 'production') {
     // Log stuff in dev as we don't want any more prod deps
     const morgan = require('morgan');
     app.use(morgan("dev"));
 }
-*/
 
 app.use(express.json());
 
@@ -29,13 +27,14 @@ app.use(express.urlencoded({extended: false}));
 // Serve our static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter)
 
 // Register our routes on new router to apply the middleware
 const apiRouter = express.Router();
 apiRouter.use(authMiddleware);
 apiRouter.use('/domain', domainRouter);
 apiRouter.use('/crawledData', crawledDataRouter);
-app.use('/api', apiRouter);*/
+app.use('/api', apiRouter);
+
 
 module.exports = app;
